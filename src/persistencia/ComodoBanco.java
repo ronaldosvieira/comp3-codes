b8424c5c2a5dba4db046315225bfe0cec49b690a
Ronaldo Vieira <ronaldo.vieira@ufrrj.br>
2016-11-04 21:33:45 -0200

    Implementação dos métodos update(1), insert(1) e remove(1)

--- src/persistencia/ComodoBanco.java
+++ src/persistencia/ComodoBanco.java
@@ -111,6 +111,7 @@ public class ComodoBanco implements AutoCloseable {
 	public int insert(Comodo comodo) throws SQLException {
 		String sql1 = "insert into comodo (descricao, tipo) values (?, ?)";
 		String sql2 = "insert into ? (comodo_id) values (?)";
+		String sql3 = "insert into comodo_mobilia (comodo_id, mobilia_id) values (?, ?)";
 
 		PreparedStatement stmt = conn.prepareStatement(sql1, Statement.RETURN_GENERATED_KEYS);
 		stmt.setString(1, comodo.obterDescricao());
@@ -127,6 +128,15 @@ public class ComodoBanco implements AutoCloseable {
 				stmt2.setInt(2, id);
 
 				stmt2.executeUpdate();
+				
+				PreparedStatement stmt3 = conn.prepareStatement(sql3);
+				
+				for (Mobilia mobilia : comodo.listaMobiliaDisponivel()) {
+					stmt3.setInt(1, id);
+					stmt3.setInt(2, mobilia.obterId());
+					
+					stmt3.executeUpdate();
+				}
 
 				return id;
 			}
@@ -138,26 +148,84 @@ public class ComodoBanco implements AutoCloseable {
 	}
 
 	public void update(int id, Comodo comodo) throws SQLException {
-		String sql = "update comodo set descricao = ? where id = ?";
+		String sql1 = "update comodo set descricao = ? where id = ?";
+		String sql2 = "select * from comodo_mobilia where comodo_id = ?";
+		String sqlInsert = "insert into comodo_mobilia (comodo_id, mobilia_id) values (?, ?)";
+		String sqlRemove = "delete from comodo_mobilia where comodo_id = ? and mobilia_id = ?";
 
-		PreparedStatement stmt = conn.prepareStatement(sql);
+		PreparedStatement stmt = conn.prepareStatement(sql1);
 		stmt.setString(1, comodo.obterDescricao());
 		stmt.setInt(2, id);
 
 		stmt.executeUpdate();
+		
+		PreparedStatement stmt2 = conn.prepareStatement(sql2);
+		stmt2.setInt(1, id);
+		
+		PreparedStatement stmtInsert = conn.prepareStatement(sqlInsert);
+		PreparedStatement stmtRemove = conn.prepareStatement(sqlRemove);
+		
+		ResultSet rs2 = null;
+		List<Integer> mobiliaIds = new ArrayList<>();
+		
+		if (stmt2.execute()) {
+			rs2 = stmt2.getResultSet();
+		}
+		
+		// obtem lista de mobilias associadas atualmente
+		while (rs2.next()) {
+			mobiliaIds.add(rs2.getInt("mobilia_id"));
+		}
+		
+		// associa as mobilias necessarias
+		for (Mobilia mobilia : comodo.listaMobiliaDisponivel()) {
+			if (!mobiliaIds.contains(mobilia.obterId())) {
+				stmtInsert.setInt(1, comodo.obterId());
+				stmtInsert.setInt(2, mobilia.obterId());
+				
+				stmtInsert.executeUpdate();
+			}
+		}
+		
+		// desassocia as mobilias necessarias
+		for (int mobiliaId : mobiliaIds) {
+			if (!comodo.listaMobiliaDisponivel().contains(mobiliaId)) {
+				stmtRemove.setInt(1, comodo.obterId());
+				stmtRemove.setInt(2, mobiliaId);
+			}
+		}
 	}
 
-	public boolean remove(int id) throws SQLException {
-		String sql = "delete from comodo where comodo_id = ?";
+	public void remove(int id) throws SQLException, IndexOutOfBoundsException {
+		String sql1 = "delete from comodo_mobilia where comodo_id = ?";
+		String sqlCC = "delete from comodo_composto_comodo where comodo_composto_id = ?";
+		String sql2 = "delete from ? where comodo_id = ?";
+		String sql3 = "delete from comodo where comodo_id = ?";
 
-		PreparedStatement stmt = conn.prepareStatement(sql);
-		stmt.setString(1, String.valueOf(id));
+		Comodo comodo = this.get(id);
+		
+		if (comodo.obterTipo().equals("comodo_composto")) {
+			PreparedStatement stmtCC = conn.prepareStatement(sqlCC);
+			stmtCC.setInt(1, comodo.obterId());
+		}
+		
+		PreparedStatement stmt = conn.prepareStatement(sql1);
+		stmt.setInt(1, id);
 
-		stmt.execute();
+		stmt.executeUpdate();
+		
+		PreparedStatement stmt2 = conn.prepareStatement(sql2);
+		stmt2.setString(1, comodo.obterTipo());
+		stmt2.setInt(2, comodo.obterId());
+		
+		stmt2.executeUpdate();
+		
+		PreparedStatement stmt3 = conn.prepareStatement(sql3);
+		stmt3.setInt(1, comodo.obterId());
+		
+		stmt3.executeUpdate();
 
 		int affectedRows = stmt.getUpdateCount();
-
-		return affectedRows == -1 ? false : true;
 	}
 
 	public void close() throws SQLException {
